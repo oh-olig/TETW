0057042b8470b7af8c6dc945f7eeec75add389ff
Oli Griffiths <35035609+oh-olig@users.noreply.github.com>
2019-06-16 19:26:19 +0100

    Page content navigation, closes #1

--- Website/js/main.js
+++ Website/js/main.js
@@ -1,10 +1,28 @@
 $(document).ready(function () {
     
     ////////////////////////////////////////////////////////////
+    // P A G E   N A V I G A T I O N
+    ////////////////////////////////////////////////////////////
+
+    // Initially Load Home Content
+    $("#content").load("content/home.html");
+
+    // Navbar Links Change Content
+    $("ul.nav li a, a.navbar-brand").on("click", function (e) {  
+
+        $("ul.nav li").removeClass("active");
+        $(this).parent().toggleClass("active");
+
+        var id = $(this).attr("id");
+
+        $("#content").load(id);
+    });
+
+    ////////////////////////////////////////////////////////////
     // A N I M A T I O N
     //////////////////////////////////////////////////////////// 
     
-    // Footer Logos Tween
+    // Tween Footer Logo Links
     TweenMax.set($("footer img"), {filter: "grayscale(100%)", autoAlpha: 0.2});
     $("footer img").on({
         mouseenter:function() {
@@ -12,82 +30,6 @@ $(document).ready(function () {
         }, mouseleave:function() {
             TweenMax.to($(this), 0.2, {filter: "grayscale(100%)", autoAlpha: 0.2});
         }
-    }); 
-    
-    ////////////////////////////////////////////////////////////
-    // S C R O L L   M A G I C
-    ////////////////////////////////////////////////////////////  
-    
-    // Init Scroll Magic Controller
-    var controller = new ScrollMagic.Controller();  
-    
-    // Animate Scroll
-    controller.scrollTo(function (newpos) {
-        newpos = newpos - 50;
-        TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
-    });
-    
-    // Bind Scroll Anchor Links
-    $("ul.nav li a, a.navbar-brand img").on("click", function (e) {  
-        
-        // Navbar Inactive/Active Link
-        $("ul.nav li").removeClass("active");
-        $(this).parent().toggleClass("active");
-        $(".navbar-collapse").removeClass("in");
-        
-        // Anchor Link ID
-        var id = $(this).attr("href");
-       
-        if ($(id).length > 0) {
-            e.preventDefault(); 
-            
-            // Trigger Scroll
-            controller.scrollTo(id);
-            
-            // Update URL
-            if (window.history && window.history.pushState) {
-                history.pushState("", document.title, id);
-            }
-        }
-    });
-
-    ////////////////////////////////////////////////////////////
-    // M O D A L
-    //////////////////////////////////////////////////////////// 
-
-    // Get the modal
-    var modal = document.getElementById("myModal");
-
-    // Get the image and insert it inside the modal - use its "alt" text as a caption
-    var img = document.getElementById("myImg");
-    var modalImg = document.getElementById("img01");
-    var captionText = document.getElementById("caption");
-    img.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-    }
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    ////////////////////////////////////////////////////////////
-    // J A W G   M A P
-    //////////////////////////////////////////////////////////// 
-    
-    // Allow map srcoll zoom after click
-    $('.map-container').click(function () {
-        $('.map-container iframe').css("pointer-events", "auto");
-    });
-    
-    // Prevent map scroll zoom
-    $( ".map-container" ).mouseleave(function() {
-      $('.map-container iframe').css("pointer-events", "none");
     });
     
 });
