c47ab3fd40cef5f8ae2afc1b10bf4d363f9a2f74
Oli Griffiths <35035609+oh-olig@users.noreply.github.com>
2020-06-22 19:25:02 +0100

    Bootstrap framework removed, complete restructure

--- Website/js/main.js
+++ Website/js/main.js
@@ -4,27 +4,28 @@ $(document).ready(function () {
     // P A G E   N A V I G A T I O N
     ////////////////////////////////////////////////////////////
 
-    // Initially Load Home Content
     $("#content").load("content/home.html");
 
-    // Navbar Links Change Content
-    $("ul.nav li a, a.navbar-brand").on("click", function (e) {  
-
-        $("ul.nav li").removeClass("active");
-        $(this).parent().toggleClass("active");
-        $(".navbar-collapse").removeClass("in");
-
+    $("header a").on("click", function (e) { 
         var id = $(this).attr("id");
-
         $("#content").load(id);
         window.scroll(0, 0);
+        $(".navbar").removeClass("responsive");
+    });
+
+    $(".icon").on("click", function (e) {
+        var navBar = document.getElementById("header-navbar");
+        if (navBar.className === "navbar") {
+            $(".navbar").addClass("responsive");
+        } else {
+            $(".navbar").removeClass("responsive");
+        }
     });
 
     ////////////////////////////////////////////////////////////
     // A N I M A T I O N
     //////////////////////////////////////////////////////////// 
     
-    // Tween Footer Logo Links
     TweenMax.set($("footer img"), {filter: "grayscale(100%)", autoAlpha: 0.2});
     $("footer img").on({
         mouseenter:function() {
