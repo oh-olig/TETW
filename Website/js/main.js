4fbafd4bab644abbe18aa47c0dd85609795a8aeb
Oli Griffiths <35035609+oh-olig@users.noreply.github.com>
2020-06-23 14:33:16 +0100

    Header resize javascript

--- Website/js/main.js
+++ Website/js/main.js
@@ -13,6 +13,10 @@ $(document).ready(function () {
         $(".navbar").removeClass("responsive");
     });
 
+    ////////////////////////////////////////////////////////////
+    // R E S P O N S I V E   N A V B A R
+    ////////////////////////////////////////////////////////////
+
     $(".icon").on("click", function (e) {
         var navBar = document.getElementById("header-navbar");
         if (navBar.className === "navbar") {
@@ -21,6 +25,25 @@ $(document).ready(function () {
             $(".navbar").removeClass("responsive");
         }
     });
+
+    ////////////////////////////////////////////////////////////
+    // R E S I Z E   H E A D E R
+    ////////////////////////////////////////////////////////////
+
+    window.onscroll = function() {resizeHeader()};
+
+    var header = document.getElementById("header-navbar");
+    var offset = header.offsetTop;
+
+    function resizeHeader() {
+        if (window.pageYOffset > offset) {
+            $(".navbar").addClass("resize");
+            $(".content").addClass("resize");
+        } else {
+            $(".navbar").removeClass("resize");
+            $(".content").removeClass("resize");
+        }
+    } 
 
     ////////////////////////////////////////////////////////////
     // A N I M A T I O N
