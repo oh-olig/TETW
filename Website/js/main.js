878ffd8aa9e6a69dd440058dfd408b278385db2e
Oli Griffiths <35035609+oh-olig@users.noreply.github.com>
2021-01-23 10:43:08 +0000

    gsap duration removed

--- Website/js/main.js
+++ Website/js/main.js
@@ -31,12 +31,12 @@ $(document).ready(function () {
     // A N I M A T I O N
     //////////////////////////////////////////////////////////// 
     
-    TweenMax.set($("footer img"), {filter: "grayscale(100%)", autoAlpha: 0.2});
+    gsap.set($("footer img"), {filter: "grayscale(100%)", autoAlpha: 0.2});
     $("footer img").on({
         mouseenter:function() {
-            TweenMax.to($(this), 0.5, {filter: "none", autoAlpha: 1});
+            gsap.to($(this), {filter: "none", autoAlpha: 1});
         }, mouseleave:function() {
-            TweenMax.to($(this), 0.2, {filter: "grayscale(100%)", autoAlpha: 0.2});
+            gsap.to($(this), {filter: "grayscale(100%)", autoAlpha: 0.2});
         }
     });
     
